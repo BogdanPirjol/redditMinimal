0c133ed6815497fc68cbf10e8efe812a0a01da62
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-02-01 22:11:20 +0200

    adaug functie helper pt calcularea timpului scurs de la data postarii

---
+++ src/app/helpers.js
@@ -0,0 +1,30 @@
+export const getDays = (createdAt) => {     //return number of days, hours, minutes or seconds from date 'createdAt' depending on input param 
+    const elpasedTime = Date.now() - createdAt;
+    const days = Math.floor(elpasedTime / 86400);
+    if(days === 0)
+    {
+        const hours = Math.floor(elpasedTime / 3600);
+        if( hours === 0){
+            const minutes = Math.floor(elpasedTime / 60);
+            if(minutes === 0)
+            {
+                return ({
+                    units: elpasedTime,
+                    type: 'seconds'
+                });
+            }
+            return ({
+                units: minutes,
+                type: 'minutes'
+            })
+        }
+        return ({
+            units: hours,
+            type: 'hours'
+        })
+    }
+    return ({
+        units: days,
+        type: 'days'
+    })
+}
