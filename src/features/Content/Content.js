bcc7d2c54f2ca9727293eecbb3cfc485fd27f6fa
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-02-05 17:48:21 +0200

    modific componenta Content -> inlatur un test

--- src/features/Content/Content.js
+++ src/features/Content/Content.js
@@ -3,13 +3,6 @@ import { Subredits } from "../Subreddits/Subredits"
 
 export const Content = () => {
 
-    /*let screenHeight = window.screen.availHeight;
-    console.log(document.body.clientHeight);
-    return <div className="responsive" style={{height: screenHeight - 200, overflow: "hidden"}}></div>;
-    */
-
-    
-
     return (
         <div className="contentContainer"> 
             <Post></Post>
