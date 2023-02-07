a3ff5554c34491c7782678ee907d95736dcdd6b8
BogdanPirjol <bogdan.pirjol123@gmail.com>
2023-02-07 13:53:35 +0200

    adaug componenta pt titlul postarii

---
+++ src/Components/Title.js
@@ -0,0 +1,10 @@
+
+export const Title = (props) => {
+    const {textTitle} = props;
+
+    return (
+        <div className='contentTitle'>
+            <p>{textTitle}</p>
+        </div>
+    );
+}
